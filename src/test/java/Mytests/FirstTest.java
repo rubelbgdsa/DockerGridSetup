2d8adf3e1a2e897a5438c32c1c38f625b3fc1a64
rubelbgdsa <73572665+rubelbgdsa@users.noreply.github.com>
2021-01-28 10:13:55 -0600

    Update FirstTest.java

--- src/test/java/Mytests/FirstTest.java
+++ src/test/java/Mytests/FirstTest.java
@@ -1,33 +1,36 @@
 package Mytests;
 
-import org.testng.Assert;
+import static org.testng.Assert.assertTrue;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Reporter;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-public class FirstTest extends BaseTest
-{
-    @Test
-    public void FacebookTest() throws Exception {
-        Reporter.log(String.format("Facebook Test Started %s",Thread.currentThread().getId()),true);
-        getDriver().navigate().to("http://www.facebook.com");
-        Assert.assertEquals(getDriver().getTitle(), "Facebook – log in or sign up");
-        Reporter.log(String.format("Facebook Test Ended %s",Thread.currentThread().getId()),true);
-    }
+public class FirstTest extends BaseTest {
+	@Test
+	public void blogPageValidation() throws Exception {
+		SoftAssert soft = new SoftAssert();
+		Reporter.log(String.format("BlogPage Test Started %s", Thread.currentThread().getId()), true);
+		getDriver().navigate().to("https://www.aumni.fund");
+		getDriver().findElement(By.xpath("//*[@id=\"comp-kd5ryhsn2label\"]")).click();
+		soft.assertTrue(getDriver().getTitle().contains("Automated inve"));
+		Reporter.log(String.format("Blog Page Validation Test Ended %s", Thread.currentThread().getId()), true);
+		soft.assertAll();
+	}
 
-    @Test
-    public void GmailTest() throws Exception {
-        Reporter.log(String.format("Gmail Test Started %s",Thread.currentThread().getId()),true);
-        getDriver().navigate().to("http://www.gmail.com");
-        Assert.assertEquals(getDriver().getTitle(), "Gmail");
-        Reporter.log(String.format("Gmail Test Ended %s",Thread.currentThread().getId()),true);
-    }
+	@Test
+	public void resourcePageValidation() throws Exception {
+		Reporter.log(String.format("resource Page Validation Test Started %s", Thread.currentThread().getId()), true);
+		getDriver().navigate().to("https://www.aumni.fund");
+		getDriver().findElement(By.xpath("//*[@id='comp-kd5ryhsn1label']")).click();
+		assertTrue(getDriver().getTitle().contains("Automated investment analyt"));
+		Reporter.log(String.format("resource Page Validation Test Ended %s", Thread.currentThread().getId()), true);
+	}
 
-    @Test
-    public void InstagramTest() throws Exception {
-        Reporter.log(String.format("Instagram Test Started %s",Thread.currentThread().getId()),true);
-        getDriver().navigate().to("https://www.instagram.com/");
-        Assert.assertEquals(getDriver().getTitle(), "Instagram");
-        Reporter.log(String.format("Instagram Test Ended %s",Thread.currentThread().getId()),true);
-    }
 }
